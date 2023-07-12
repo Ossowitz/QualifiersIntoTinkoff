dedf478f1e6d087ab8c0e640a8412931e3b06f99
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-07-13 00:17:23 +0300

    9. Palindrome Number

---
+++ Leetcode/src/main/java/Lesson2/Solution.java
@@ -0,0 +1,20 @@
+package Lesson2;
+
+public class Solution {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int reversedNumber = 0;
+        int originNumber = x;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            x /= 10;
+        }
+
+        return originNumber == reversedNumber;
+    }
+}
---
+++ Leetcode/src/main/test/Lesson2/SolutionTest.java
@@ -0,0 +1,37 @@
+package Lesson2;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class SolutionTest {
+
+    @Test
+    public void testIsPalindrome() {
+        int number = 121;
+        boolean expected = true;
+
+        boolean actual = Solution.isPalindrome(number);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsPalindromeNegativeNumber() {
+        int number = -121;
+        boolean expected = false;
+
+        boolean actual = Solution.isPalindrome(number);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsPalindromeNotPalindromeNumber() {
+        int number = 10;
+        boolean expected = false;
+
+        boolean actual = Solution.isPalindrome(number);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
