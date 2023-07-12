d478131414c0db92833b68e1110b19900c2e6873
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-07-13 00:16:48 +0300

    2. Two sum

---
+++ Leetcode/src/main/test/Lesson1/SolutionTest.java
@@ -0,0 +1,21 @@
+package Lesson1;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class SolutionTest {
+
+    @Test
+    public void testTwoSum() {
+        // Test value
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expected = {0, 1};
+
+        // Call a method from the Solution class
+        int[] result = Solution.twoSum(nums, target);
+
+        // Check expected value
+        Assertions.assertArrayEquals(expected, result);
+    }
+}
