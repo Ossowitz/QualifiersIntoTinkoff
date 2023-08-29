17d370b5e5cf90cd3637cf779df29a7685125623
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-08-29 14:32:56 +0300

    Solution for SecondTinkoff

---
+++ Leetcode/src/main/java/tink/TinkSecond.scala
@@ -0,0 +1,27 @@
+package tink
+
+object TinkSecond {
+  def main(args: Array[String]): Unit = {
+    printNumbers(4, 3)
+  }
+
+  private def printNumbers(n: Int, m: Int): Unit = {
+    val numbers = Array.ofDim[Int](m, n)
+    var counter = 1
+    for (i <- 0 until m + n - 1) {
+      for (j <- 0 to i) {
+        if (j < m && i - j < n) {
+          numbers(j)(i - j) = counter
+          counter += 1
+        }
+      }
+    }
+
+    for (row <- numbers) {
+      for (num <- row) {
+        print(num + " ")
+      }
+      println()
+    }
+  }
+}
